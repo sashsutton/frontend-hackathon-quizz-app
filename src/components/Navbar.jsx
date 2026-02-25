b7709408aa4008dba09a37dd470e4fba7ebc43ce
sashasutton <sashasuttons3@gmail.com>
2026-02-25 15:41:07 +0100

    Refactor Navbar styles and structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,15 +5,15 @@ function Navbar() {
     const isConnected = false;
 
     return(
-        <nav className="nav-crumbs">
+        <nav className="navbar">
         
-        <ul className="">
-            <li class="nav-crumb"><a href="/">Accueil</a></li>
-            <li class="nav-crumb"><a href="/play">Jouer</a></li>
-            <li class="nav-crumb"><a href="/make">Créer</a></li>
-            <li class="nav-crumb"><a href="/quiz-list">Liste des quiz</a></li>
-            {!isConnected && <li class="nav-crumb"><a href="/login">Se connecter</a></li>}
-            {isConnected && <li class="nav-crumb"><a href="/logout">Se déconnecter</a></li>}
+        <ul className="nav-crumbs">
+            <li className="nav-crumb"><a href="/">Accueil</a></li>
+            <li className="nav-crumb"><a href="/play">Jouer</a></li>
+            <li className="nav-crumb"><a href="/make">Créer</a></li>
+            <li className="nav-crumb"><a href="/quiz-list">Liste des quiz</a></li>
+            {!isConnected && <li className="nav-crumb"><a href="/login">Se connecter</a></li>}
+            {isConnected && <li className="nav-crumb"><a href="/logout">Se déconnecter</a></li>}
         </ul>
         </nav>
 
