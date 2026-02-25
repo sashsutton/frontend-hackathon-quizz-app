d10ad7e7d158b4e20b99eb5b485f10393baa245e
Bjorn Lai <bjornlai13@gmail.com>
2026-02-25 16:45:19 +0100

    Affichage de la liste des quiz

--- src/pages/QuizListPage.jsx
+++ src/pages/QuizListPage.jsx
@@ -1,8 +1,19 @@
 
 function QuizList() {
+    const list_of_quiz = ["placeholder_quiz1", "placeholder_quiz2", "placeholder_quiz3"];
+
     return (
         <div>
             <h1>Liste des quiz</h1>
+
+            <ul>
+                {list_of_quiz.map((item, index) => (
+                    <li key={index}><a href="">{item}</a></li>
+                ))
+                }   
+                
+            </ul>
+
         </div>
     )
 }
