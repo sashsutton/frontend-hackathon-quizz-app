d77f0592fed61a07c12f24e6c5a9c508036a9e6c
sashasutton <sashasuttons3@gmail.com>
2026-02-25 12:15:54 +0100

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css'
-
+import {Routes, Route} from "react-router-dom"
 import Navbar from './components/Navbar.jsx'
+import HomePage from "./pages/HomePage.jsx";
 
 function App() {
 
   return (
       <div>
           <h1>Hackathon 2026</h1>
-          <Navbar />
+          <Navbar></Navbar>
+          <Routes>
+              <Route path="/" element={<HomePage/>}/>
+
+          </Routes>
 
       </div>
   )
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+
+
+
+function HomePage() {
+    return(
+        <div>
+            <h1>Home Page</h1>
+            <h2>Hackathon 2026</h2>
+        </div>
+    )
+}
+
+export default HomePage
