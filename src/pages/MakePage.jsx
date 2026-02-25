65df4e2da9565250724dbe052a238d9b090e44b1
Bjorn Lai <bjornlai13@gmail.com>
2026-02-25 17:38:35 +0100

    Création de quiz, vérification de validité du fichier (format correct)

--- src/pages/MakePage.jsx
+++ src/pages/MakePage.jsx
@@ -1,16 +1,45 @@
+import { useState } from "react"
 
 function MakePage() {
+
+    const [valid, setValid] = useState(true);
+
+    const handleFileChange = (e) => {
+        const file = e.target.files[0];
+        const reader = new FileReader();
+        reader.onload = (ev) => {
+            const lines = ev.target.result.split("\n");
+            
+            let isValid = true;
+
+            for (const line of lines) {
+                const trimmed = line.trim();
+
+                if (trimmed === "") continue; // Ligne vide
+
+                const fields = line.split(";");
+                if (fields.length !== 6) isValid = false;
+            }
+            
+            setValid(isValid);
+            console.log("Il est " + valid + " que le fichier est valide");
+        } 
+        reader.readAsText(file);
+    }
+
+
     return (
         <div>
             <h1>Créer un Quiz</h1>
             <p>
             Vous pouvez créer un quiz en important un fichier csv respectant le format suivant : <br/>
-            question;réponse_1;réponse_2;réponse_3;réponse_4
+            question;réponse_1;réponse_2;réponse_3;réponse_4;bonnes_réponses
             </p>
 
             <form action="/make-quiz" method="POST">
-                <input type="file" id="quiz-import" name="quiz-import" accept=".csv" required></input>
-                <input type="submit" value="Créer"></input>
+                <input type="file" id="quiz-import" name="quiz-import" accept=".csv" required onChange={handleFileChange}></input>
+                {valid && <input type="submit" value="Créer"></input>}
+                {!valid && <p>Fichier non valide, vérifiez qu'il respecte bien le format décrit ci-dessus</p>}
             </form>
         </div>
     )
