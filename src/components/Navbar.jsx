2267bba1ce18d6df27d0bfdfb5ddc9c79e64a16a
Bjorn Lai <bjornlai13@gmail.com>
2026-02-25 13:43:49 +0100

    Implémentation de la navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,19 @@
 
 
 
-function Navbar(){
+function Navbar() {
+    const isConnected = true;
+
     return(
-        <div>
-            <p> This is my navbar</p>
-        </div>
+        <nav className="crumbs">
+        
+        <ul className="">
+            <li class="crumb"><a href="./play">Jouer</a></li>
+            <li class="crumb"><a href="./make">Créer</a></li>
+            {!isConnected && <li class="crumb"><a href="./login">Se connecter</a></li>}
+            {isConnected && <li class="crumb"><a href="./logout">Se déconnecter</a></li>}
+        </ul>
+        </nav>
 
     )
 }
