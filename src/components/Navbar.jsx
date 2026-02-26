5367620357de00961414488a5eb1e7435c568f30
sashasutton <sashasuttons3@gmail.com>
2026-02-27 00:11:10 +0100

    Navbar changed to have a logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,16 +4,28 @@ import { Link } from 'react-router-dom';
 function Navbar() {
     return (
         <nav className="navbar">
+            <div className="nav-brand">
+                <Link to="/" style={{ textDecoration: 'none' }}>
+                    <span className="nav-logo">⚡ QuizApp</span>
+                </Link>
+            </div>
             <ul className="nav-crumbs">
-                <li className="nav-crumb"><Link to="/">Accueil</Link></li>
                 <li className="nav-crumb"><Link to="/quiz-list">Quiz</Link></li>
                 <li className="nav-crumb"><Link to="/duel">⚔️ Duel</Link></li>
                 <li className="nav-crumb"><Link to="/leaderboard">🏆 Classement</Link></li>
                 <li className="nav-crumb"><Link to="/make">Créer</Link></li>
 
                 <SignedOut>
-                    <li className="nav-crumb"><SignInButton mode="modal" /></li>
-                    <li className="nav-crumb"><SignUpButton mode="modal" /></li>
+                    <li className="nav-crumb">
+                        <SignInButton mode="modal">
+                            <button className="nav-btn nav-btn-outline">Se connecter</button>
+                        </SignInButton>
+                    </li>
+                    <li className="nav-crumb">
+                        <SignUpButton mode="modal">
+                            <button className="nav-btn nav-btn-primary">S'inscrire</button>
+                        </SignUpButton>
+                    </li>
                 </SignedOut>
 
                 <SignedIn>
